7e5c56191b688d6e970e600908c480caa113ca27
coder2056 <152678726+coder2056@users.noreply.github.com>
2024-03-07 23:17:50 +0530

    ini

---
+++ Assets/scripts/DragTransform.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+
+class DragTransform : MonoBehaviour
+{
+    private Color mouseOverColor = Color.green;
+    private Color originalColor = Color.red;
+    private bool dragging = false;
+    private float distance;
+
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = mouseOverColor;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = originalColor;
+    }
+
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+    }
+
+    void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint;
+        }
+    }
+}
